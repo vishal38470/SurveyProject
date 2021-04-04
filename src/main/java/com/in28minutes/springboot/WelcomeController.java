6a81090a5c886eb9ffe17c4271c65941615dfddc
saurabhparmar08\Owner <vishal.joshi.testing@gmail.com>
2021-04-04 11:27:21 +0530

    step 10 commit

---
+++ src/main/java/com/in28minutes/springboot/WelcomeController.java
@@ -0,0 +1,18 @@
+package com.in28minutes.springboot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+	//Auto wiring
+	@Autowired
+	private WelcomeService service;
+
+	@RequestMapping("/welcome")
+	public String welcome() {
+		return service.retrieveWelcomeMessage();
+	}
+}
---
+++ src/main/java/com/in28minutes/springboot/WelcomeService.java
@@ -0,0 +1,12 @@
+package com.in28minutes.springboot;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class WelcomeService {
+
+	public String retrieveWelcomeMessage() {
+		//Complex Method
+		return "Good Morning updated";
+	}
+}
